43a94c3127725dec9703701db8b1cbb5d8e765b9
JunhuaLin <630361743@qq.com>
2017-03-19 21:51:32 +0800

    finish api of packaging_tools

--- fun/packaging_tools.py
+++ fun/packaging_tools.py
@@ -1,5 +1,8 @@
 # coding=utf-8
 import os
+import re
+import shutil
+import zipfile
 
 
 class ChannelParser(object):
@@ -9,7 +12,7 @@ class ChannelParser(object):
     def parse(self, path):
         self.channels = []
         if not (path and os.path.exists(path)):
-            return self.channels
+            return []
 
         try:
             if os.path.isfile(path):
@@ -30,6 +33,7 @@ class ChannelParser(object):
             return []
 
         temp_channels = []
+        patten = re.compile(r"[\w]+")
         with open(path, "r") as f:
 
             while True:
@@ -39,7 +43,7 @@ class ChannelParser(object):
 
                 for line in content:
                     line = line.strip()
-                    if line:
+                    if line and patten.match(line):
                         temp_channels.append(line)
 
         return temp_channels
@@ -65,6 +69,50 @@ class ChannelParser(object):
         return temp_channels
 
 
+class PackagingApk(object):
+    def __init__(self, apk_path):
+        self.apk_path = apk_path
+        self.channel_list = []
+        self.output_path = ""
+
+    @staticmethod
+    def instance(apk_path):
+        return PackagingApk(apk_path)
+
+    def channels(self, channel_path):
+        self.channel_list = ChannelParser().parse(channel_path)
+        return self
+
+    def dist(self, output_path):
+        self.output_path = output_path
+        return self
+
+    def pack(self, prefix="", suffix=""):
+        """
+        向apk中写入META-INF/channel_{channel}空文件，channel为ascii渠道名
+        :param prefix:
+        :param suffix:
+        :return:
+        """
+        if not self.output_path:
+            output = os.path.split(self.apk_path)
+            apk_dir = os.path.join(output[0], "apk")
+            if not os.path.exists(apk_dir):
+                os.makedirs(apk_dir)
+            self.output_path = apk_dir
+
+        for channel in self.channel_list:
+            output_apk = os.path.join(self.output_path,
+                                      "".join([prefix, channel, suffix, ".apk"]))
+            shutil.copyfile(self.apk_path, output_apk)
+
+            with zipfile.ZipFile(output_apk, 'a', zipfile.ZIP_DEFLATED) as zipped:
+                empty_channel_file = "META-INF/channel_{channel}".format(channel=channel)
+                zipped.write(self.apk_path, empty_channel_file)
+
+
 if __name__ == "__main__":
-    cp = ChannelParser()
-    print cp.parse(r"D:\channel\1.txt")
+    your_apk = r"D:\channel\app-debug.apk"
+    channel_file = r"D:\channel"
+
+    PackagingApk.instance(your_apk).channels(channel_file).pack()
