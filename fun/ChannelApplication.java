f09b65a6505fd6cc451835aa19f6d74bfd098199
junhua <kim@suin.us>
2017-03-22 18:48:28 +0800

    add doc

--- fun/ChannelApplication.java
+++ fun/ChannelApplication.java
@@ -1,14 +1,16 @@
-# MultiChannelApk
-PackagingTools适用于多渠道android安装包apk生成，该工具的实现是基于美团分享的打包方式（[原文传送门](http://tech.meituan.com/mt-apk-packaging.html)）。简单，快捷，效率高。
+package cn.junhua.android.linjhandroid;
 
-## 使用环境
-先要配置Python2.7（ubuntu和MacOS默认即可），因为PackagingTools运行在Python2.7上。
+import android.app.Application;
+import android.content.pm.ApplicationInfo;
 
-## 快速使用
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
-### 代码
-Android中加入如下代码：
-```java
+/**
+ * Created by junhua on 17-3-22.
+ */
 public class ChannelApplication extends Application {
 
     private static ChannelApplication INSTANCE;
@@ -18,6 +20,8 @@ public class ChannelApplication extends Application {
     public void onCreate() {
         super.onCreate();
         INSTANCE = this;
+
+        getChannel();
     }
 
     /**
@@ -70,38 +74,3 @@ public class ChannelApplication extends Application {
         return CHANNEL;
     }
 }
-```
-
-
-### 打包
-#### 1.下载源文件
-进入../MultiChannelApk/fun/目录运行：
->python packaging_tools.py -h
-
-输出帮助信息：
-
-![帮助信息](https://github.com/JunhuaLin/MultiChannelApk/blob/master/assets/help.png)
-
-按着帮助信息填写参数。
-
-#### 2.设置源和渠道信息
-
-![执行生成](https://github.com/JunhuaLin/MultiChannelApk/blob/master/assets/gen.png)
-
-- a 源：第一个位置参数 /home/junhua/channel/app-debug.apk
-- c 渠道信息：/home/junhua/channel/ 即该目录下所有的*.channel文件
-
-#### 3.生成文件
-
-![生成文件](https://github.com/JunhuaLin/MultiChannelApk/blob/master/assets/result1.png)
-
-![APK内部文件](https://github.com/JunhuaLin/MultiChannelApk/blob/master/assets/inner.png)
-
-### 使用Channel信息
-
->ChannelApplication.getChannel()
-
-
-
-
-
